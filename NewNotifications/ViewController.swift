ab58612b5fccd1281ed2dad47e6fa0ec42c31e11
Aman Vaid <amanvaid94@gmail.com>
2017-05-23 22:27:50 -0700

    Adding all files and some extra pictures

--- NewNotifications/ViewController.swift
+++ NewNotifications/ViewController.swift
@@ -7,19 +7,72 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //1. Request Permission
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { (granted, error) in
+            
+            if granted {
+                print("Notification Access Granted")
+            } else {
+                print(error?.localizedDescription as Any)
+            }
+        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func notifyButtonTapped(sender: UIButton){
+        scheduleNotification(inSeconds: 5, completion: { success in
+            if success {
+                print("Successfully scheduled notification")
+            } else {
+                print("Error scheduling notification")
+            }
+        })
     }
-
-
+    
+    func scheduleNotification(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()) {
+        
+        //Add attachment
+        let myImage = "superman3"
+        guard let imageUrl = Bundle.main.url(forResource: myImage, withExtension: "jpg") else {
+            completion(false)
+            return
+        }
+        
+        var attachment: UNNotificationAttachment
+        
+        attachment = try! UNNotificationAttachment(identifier: "myNotification", url: imageUrl, options: .none)
+        
+        let notif = UNMutableNotificationContent()
+        
+        //Only for extension
+        notif.categoryIdentifier = "myNotificationCategory"
+        
+        notif.title = "What is that?!"
+        notif.subtitle = "Up in the sky?"
+        notif.body = "Is it a bird. Is it a plane? ... NO! It's Superman!"
+        
+        notif.attachments = [attachment]
+        
+        let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notifTrigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if error != nil {
+                print(error as Any)
+                completion(false)
+            } else {
+                completion(true)
+            }
+        
+        })
+    }
 }
 
